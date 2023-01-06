1fcab2ec0f0a661f69605299e06f9d5729dbcb52
Luiz-om <luizfernando_martins@hotmail.com>
2023-01-05 20:57:43 -0400

    Iniciando Projeto

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.WriteLine("Primeiro Valor");
+float valor1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Segundo Valor");
+float valor2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
