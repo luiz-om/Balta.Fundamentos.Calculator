39d872e26f034beff404d1ca91e38896b2048f6e
Luiz-om <luizfernando_martins@hotmail.com>
2023-01-09 23:09:27 -0400

    Metodo Subtração

--- Program.cs
+++ Program.cs
@@ -1,14 +1,34 @@
-﻿Console.Clear();
+﻿Soma();
+Thread.Sleep(2000);
+Subtracao();
 
-Console.WriteLine("Primeiro Valor");
-float valor1 = float.Parse(Console.ReadLine());
+static void Soma(){
+    Console.Clear();
 
-Console.WriteLine($"Segundo Valor");
-float valor2 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Primeiro Valor");
+    float valor1 = float.Parse(Console.ReadLine());
 
-float resultado = valor1 + valor2;
+    Console.WriteLine($"Segundo Valor");
+    float valor2 = float.Parse(Console.ReadLine());
 
-Console.WriteLine($"O resultado da soma é {resultado}");
-Console.WriteLine($"O resultado da soma é " +(valor1+valor2));
+    float resultado = valor1 + valor2;
 
+    Console.WriteLine($"O resultado da soma é {resultado}");
+    // Console.WriteLine($"O resultado da soma é " + (valor1 + valor2));
+
+}
+
+static void Subtracao(){
+
+    Console.Clear();
+    Console.WriteLine($"Primeiro numero");
+    float numero1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Segundo numero");
+    float numero2 = float.Parse(Console.ReadLine());
+
+
+    float resultado = numero1 + numero2;
+    Console.WriteLine($"O resultado da soma é {resultado}");
+}
 
